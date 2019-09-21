089f692ac962a7849a24d8e852bc22944f8f84d6
BinaryWiz <utlimi450@gmail.com>
2019-09-21 17:13:41 -0400

    added more documentation

--- cache_manager.py
+++ cache_manager.py
@@ -5,47 +5,34 @@ import json
 import os
 import plyvel
 import requests
-# Sample data in the cache
-"""
-cache_data {
-    {
-        {
-            'identifier': 'BX80684I99900K;
-            'amazon_data': ['Amazon', '$484.99', 'https://www.amazon.com/s?k=BX80684I99900K&i=electronics&rh=n%3A172282&qid=1563197272&ref=sr_hi_1'], 
-            'bestbuy_data': [], 
-            'newegg_data': ['Newegg', '525', '#'], 
-            'walmart_data': ['Walmart', '$474.99', 'https://www.walmart.com/ip/Processor-Series-Lake-Coffee-Intel-UHD-630-16-Thread-Turbo-300-5-0-Core-BX80684I99900K-LGA-i9-9900K-1151-Graphics-8-Core-Desktop-GHz-3-6-95W/701836320'], 
-            'bandh_data': ['B&H', '$484.99', 'https://www.bhphotovideo.com/c/product/1435917-REG/intel_bx80684i99900k_core_i9_9900k_3_6_ghz.html'],
-            'ebay_data': ['Ebay', '$549.00', 'https://www.ebay.com/sch/i.html?_odkw=BX80684I99900K&_osacat=0&_from=R40&_trksid=p2045573.m570.l1313.TR1.TRC0.A0.H0.TRS1&_nkw=BX80684I99900K&_sacat=0'], 
-            'tigerdirect_data': ['Tiger Direct', '$565.99', 'http://www.tigerdirect.com/applications/SearchTools/item-details.asp?EdpNo=5956417&CatId=12405'], 
-            'microcenter_data': ['Microcenter', '$449.99', 'https://www.microcenter.com/product/512483/core-i9-9900k-coffee-lake-36-ghz-lga-1151-boxed-processor'], 
-            'jet_data': ['Jet', 'Could Not Find Price', 'None'], 
-            'outletpc_data': ['OutletPC', '$358.90', 'https://www.outletpc.com/km3530.html'], 
-            'superbiiz_data': ['SuperBiiz', '', 'None']
-        }
-    }
-}
-"""
+from apscheduler.schedulers.background import BackgroundScheduler
 
 request_db = None
 time_db = None
 app = Flask(__name__)
 TIME_DELETIION = 60
 
-from apscheduler.schedulers.background import BackgroundScheduler
-from flask import Flask
-
 def time_updater():
-    """ Function for test purposes. """
-    for key, value in time_db:
-        if value == bytes([TIME_DELETIION]):
-            time_db.delete(key)
-            requests.delete('http://localhost:5001/', json={'identifier': str(key)})
+    """
+    Updates how long each entry has been in the database.
+    Deletes after the amount of minutes have passed as
+    defined in TIME_DELETION
+    """
+    try:
+        for key, value in time_db:
+            if value == bytes([TIME_DELETIION]):
+                time_db.delete(key)
+                requests.delete('http://localhost:5001/', json={'identifier': str(key)})
 
-        else:
-            time_db.put(key, bytes([int.from_bytes(value, byteorder='big') + int.from_bytes([1], byteorder='big')])) 
+            else:
+                # Adds one using the bytes representation of integers
+                time_db.put(key, bytes([int.from_bytes(value, byteorder='big') + 
+                    int.from_bytes([1], byteorder='big')])) 
+    
+    except Exception as e:
+        print(str(e))
 
-
+# Schedules the time_updater function to run every minute
 sched = BackgroundScheduler(daemon=True)
 sched.add_job(time_updater,'interval',minutes=1)
 sched.start()
--- request_tester.py
+++ request_tester.py
@@ -4,17 +4,17 @@ import json
 while True:
     user_input = input("Would you like to GET PUT or DELETE? ")
 
-    if user_input == "GET":
+    if user_input.upper() == "GET":
         r = requests.get(url = "http://localhost:5001?item_model=BX80684I99900K") 
         print(r.json())
         
-    elif user_input == "PUT":
+    elif user_input.upper() == "PUT":
         data = requests.get("http://timeless-apps.com/api/query?retailer=Newegg&price=525&item_model=BX80684I99900K&return_type=json")
         load = data.json()
         print(load)
         r = requests.put(url = "http://localhost:5001/", json = load) 
         
-    elif user_input == "DELETE":
+    elif user_input.upper() == "DELETE":
         data = requests.delete("http://localhost:5001/", json={'identifier': 'BX80684I99900K'})
         response = data.json()
         print(response)
